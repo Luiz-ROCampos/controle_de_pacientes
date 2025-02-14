f53a046ca0c8db2bad1ad16e517f6dacddf0f9c0
Luiz-ROCampos <ricardoxluiz88@gmail.com>
2025-02-14 17:36:53 -0300

    Commit: app Pacientes

---
+++ pacientes/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.pacientes, name='pacientes'),
+    path('<int:id>', views.paciente_view, name='paciente_view'),
+    path('atualizar_paciente/<int:id>', views.paciente_view, name='paciente_view'),
+]
